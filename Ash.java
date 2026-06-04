fae973c9793fcd9acbc1a1ef87a654b095c70aba
Tugo <110765404+Tuguldurro@users.noreply.github.com>
2026-06-04 15:42:58 -0700

    Update Ash.java

--- Ash.java
+++ Ash.java
@@ -1,7 +1,13 @@
 
 //This file shows what happens after tree burns out
- 
+ public class Ash extends Cell {
+    public Ash() {
+        super("ash");
+    }
 
+    public void update(Forest forest, int row, int col) {
+    }
+}
    //create a class that Ash extends Plant and the constructor that super "ash"
   // create a method void update with Forest forest, int row, int col. leave inside empty
 

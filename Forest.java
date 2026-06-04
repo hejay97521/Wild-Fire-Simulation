38ddf06735d693e9199b269f217df405a7a69b07
Tugo <110765404+Tuguldurro@users.noreply.github.com>
2026-06-04 15:55:20 -0700

    Update Forest.java

--- Forest.java
+++ Forest.java
@@ -23,5 +23,53 @@ public class Forest {
             }
         }
     }
-//STILL WORKING
+    public void setOnFire(int row, int col) {
+        if (grid[row][col].getStatus().equals("tree")) {
+            grid[row][col] = new BurningTree();
+        } else if (grid[row][col].getStatus().equals("burning")) {
+            grid[row][col] = new Ash();
+        }
+    }
+
+    public Cell getCell(int row, int col) {
+        return grid[row][col];
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getCols() {
+        return cols;
+    }
+
+    public void update() {
+        Cell[][] next = new Cell[rows][cols];
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                next[r][c] = grid[r][c];
+            }
+        }
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                grid[r][c].update(this, r, c);
+            }
+        }
+        grid = next;
+    }
+    
+    public String toString() {
+        String result = "";
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                String s = grid[r][c].getStatus();
+                if (s.equals("tree"))    result += "T ";
+                else if (s.equals("burning")) result += "B ";
+                else if (s.equals("ash"))     result += "A ";
+                else                          result += ". ";
+            }
+            result += "\n";
+        }
+        return result;
+    }
 }
